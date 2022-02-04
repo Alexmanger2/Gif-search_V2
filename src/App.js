5dce22f3051299b8431231c9576074857982649f
Alexmanger2 <alexmanger2@gmail.com>
2022-02-03 20:13:33 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React,{useState,Fragment} from 'react';
+import './App.css';
+import Trending from './components/Trending';
+
+function App() {
+  return (
+    <Fragment>
+    
+        <Trending/>
+
+
+    </Fragment>
+  );
+}
+
+export default App;
